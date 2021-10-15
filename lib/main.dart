fc0d563a8142ecb75e03cd86c884da5808c33d58
jccallves <jccallves@macae.ufrj.br>
2021-10-15 13:08:07 -0300

    #Aula106 - Usando textfield #02

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -90,11 +93,13 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(
                       labelText: 'Título',
                     ),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(
                       labelText: 'Valor (R\$)',
                     ),
@@ -105,7 +110,10 @@ class MyHomePage extends StatelessWidget {
                       TextButton(
                         child: Text('Nova transação'),
                         style: TextButton.styleFrom(primary: Colors.purple),
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                       ),
                     ],
                   ),

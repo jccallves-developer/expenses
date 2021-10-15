ea600a3b72041688471575a85599165ebdd34fa9
jccallves <jccallves@macae.ufrj.br>
2021-10-15 10:00:52 -0300

    #Aula94 - Mapear dados para widgets

--- lib/main.dart
+++ lib/main.dart
@@ -43,8 +43,12 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Text('Lista de transações'),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Text('Lista de transações'),
+                );
+              }).toList(),
             )
           ],
         ));

39a29ba346c23a8c8d6a86017200abdb972f1aaa
jccallves <jccallves@macae.ufrj.br>
2021-10-15 10:21:45 -0300

    #Aula98 - Aplicando estilo no texto

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,16 @@ class MyHomePage extends StatelessWidget {
                           EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                       decoration: BoxDecoration(
                           border: Border.all(
-                        color: Colors.black,
+                        color: Colors.purple,
                         width: 2,
                       )),
                       padding: EdgeInsets.all(10),
-                      child: Text(tr.value.toString()),
+                      child: Text(tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          )),
                     ),
                     Column(
                       children: [

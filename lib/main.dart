0678130196f3248ff445df58100a8ec18607afb8
jccallves <jccallves@macae.ufrj.br>
2021-10-16 12:26:06 -0300

    #Aula117 - Fechando formulario (Modal)

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {

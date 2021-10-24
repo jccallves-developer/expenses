fa62b9d1a7edfdec973e64706759efbf7821513b
jccallves <jccallves@macae.ufrj.br>
2021-10-24 13:39:46 -0300

    #Aula154 - Objeto MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -94,8 +94,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text('Despesas pessoais'),
@@ -116,9 +116,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availabelHeight = MediaQuery.of(context).size.height -
+    final availabelHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
